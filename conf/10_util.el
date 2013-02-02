fc2410c1ca46ea460e5ed597a1b4bd39549ae3a5
taro_tnk <taro.tanaka004@gmail.com>
2013-02-03 02:29:26 +0900

    typester先生の設定を真似した部分を追記

--- conf/10_util.el
+++ conf/10_util.el
@@ -1,8 +1,8 @@
 ;; start server
 (require 'server)
-;(when (not (eq t (server-running-p server-name)))
 (unless (server-running-p)
-(server-start))
+  (server-start))
+
 
 ;; 履歴の数を設定
 (setq history-length 10000)
@@ -19,3 +19,34 @@
 
 ;; ウィンドウの上部に現在の関数名を表示する
 (which-func-mode 1)
+
+;; リージョンをC-hで削除
+(delete-selection-mode 1)
+
+;; % で対応する括弧に移動
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+(define-key global-map (kbd "%") 'match-paren)
+
+;; 色つける
+(require 'font-lock)
+(global-font-lock-mode t)
+(setq-default transient-mark-mode t)
+
+;; 一行ずつスクロール
+(setq scroll-step 1)
+
+;; 縦分割とかでも行を折り返す
+(setq truncate-partial-width-windows nil)
+;; 折り返しトグル
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+
+;; recenf-mode
+(recentf-mode t)
+
+;; local-variables safe なのは自動で適用
+(setq enable-local-variables :safe)
