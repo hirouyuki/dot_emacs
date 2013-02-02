c2ec669b13ac4b17cf7e408738c047c492e00010
taro_tnk <taro.tanaka004@gmail.com>
2013-02-03 05:35:55 +0900

    add helm.el

---
+++ conf/30_helm.el
@@ -0,0 +1,8 @@
+;;helm settging
+;(add-to-list 'load-path "~/.emacs.d/elpa/helm")
+;(require 'helm-config)
+;(require 'helm-descbinds)
+(helm-mode 1)
+(global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-c y") 'helm-show-kill-ring)
+(global-set-key (kbd "M-r") 'helm-resume)
