d1066a2faf099757d8f94197630c25e055b103ac
taro_tnk <taro.tanaka004@gmail.com>
2013-02-03 02:30:19 +0900

    typester先生のdisplay部分を真似た

--- conf/10_display.el
+++ conf/10_display.el
@@ -8,27 +8,32 @@
 (scroll-bar-mode 0)
 ;; tool-barを非表示。
 (tool-bar-mode 0)
-;; カーソルをブロックに
+;; カーソル点滅しないように
 (blink-cursor-mode 0)
+;; 行数、列数を表示
+(line-number-mode t)
+(column-number-mode t)
+
 ;; メニューバーにファイルパスを表示する
 (setq frame-title-format
       (format "%%f - Emacs@%s" (system-name)))
+
 ;; paren-mode 対応する括弧を強調して表示する
 (setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
-(show-paren-mode t)
+(show-paren-mode 1)
 ;; paren のスタイル：expression は括弧内も強調表示
 (setq show-paren-style 'expression)
 ;; 行末の空白を強調表示
 (setq-default show-trailing-whitespace t)
 (set-face-background 'trailing-whitespace "#b14770")
 
+
+
 ;;; インデントの設定
-;; タブをスペースに変換
-(setq-default indent-tabs-mode nil)
-;; 何か
-(setq-default c-basic-offset 4)
-;; タブの幅を4スペース分にする
-(setq tab-width 4)
+(setq-default c-basic-offset 4     ;;基本インデント量4
+              tab-width 4          ;;タブ幅4
+              indent-tabs-mode nil)  ;;インデントをタブでするかスペースでするか
+
 ;; 何か
 (c-set-offset 'case-label '+)
 
@@ -55,3 +60,15 @@
   (set-fontset-font
     nil 'japanese-jisx0208
     (font-spec :family "Hiragino Maru Gothic Pro")))
+
+;; C-t でother-window、分割されてなかったら分割
+(defun other-window-or-split () ; http://d.hatena.ne.jp/rubikitch/20100210/emacs
+  (interactive)
+  (when (one-window-p)
+    (split-window-horizontally))
+  (other-window 1))
+(define-key global-map (kbd "C-t") 'other-window-or-split)
+
+;; uniquify 同一ファイル名のバッファ名をわかりやすく
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
