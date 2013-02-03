26da251d419c2cfbd0cfbeee9524c16c3e4526bb
taro_tnk <taro.tanaka004@gmail.com>
2013-02-03 17:28:34 +0900

    add helm-git

--- conf/30_helm.el
+++ conf/30_helm.el
@@ -6,3 +6,6 @@
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "C-c y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-r") 'helm-resume)
+(when (require 'helm-git nil t)
+    (global-set-key (kbd "C-x C-g") 'helm-git-find-files)
+    )
