79fb7f23837a1ae0e96dbeae8ef01960a99651ed
taro_tnk <taro.tanaka004@gmail.com>
2013-02-19 13:31:29 +0900

    rubyモードでのインデントと自動括弧挿入をコメントアウト

--- conf/40_ruby.el
+++ conf/40_ruby.el
@@ -8,18 +8,18 @@
 ;; ruby-mode
 (require 'ruby-mode)
 (require 'ruby-electric)
-(add-hook 'ruby-mode-hook (lambda ()
-    (smartparens-global-mode -1)
-    (ruby-electric-mode t)
-    (electric-indent-mode -1)
-    (electric-pair-mode -1)))
-(define-key ruby-mode-map "\C-m" 'ruby-reindent-then-newline-and-indent)
+;(add-hook 'ruby-mode-hook (lambda ()
+;    (smartparens-global-mode -1)
+;    (ruby-electric-mode t)
+;    (electric-indent-mode -1)
+;    (electric-pair-mode -1)))
+;;(define-key ruby-mode-map "\C-m" 'ruby-reindent-then-newline-and-indent)
 
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 
-(setq ruby-indent-level 2
-      ruby-deep-indent-paren-style nil
-      ruby-indent-tabs-mode t)
+;(setq ruby-indent-level 2
+;      ruby-deep-indent-paren-style nil
+;      ruby-indent-tabs-mode t)
 
 ;; ruby-block.el
 (require 'ruby-block)
